6cc875ac92f0d0256a3ef16f0b2f8012d50c6219
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-07-06 17:12:19 +0530

    now the popup and bottom sheet can be shown

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:universal_event/sdk.dart';
 
 import 'Page1.dart';
 import 'Page2.dart';
@@ -12,7 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Page1(),
+      home: Scaffold(
+        key: scaffoldKey,
+        body: Page1()),
       routes: {
         '/page2': (context) => Page2(),
         '/page3': (context) => Page3(),
@@ -20,7 +23,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
--- lib/sdk.dart
+++ lib/sdk.dart
@@ -12,12 +12,83 @@ void toastMessage(String message) {
       fontSize: 14.0);
 }
 
+GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+void showBottomSheetFromAnywhere() {
+  final context = scaffoldKey.currentContext;
+  if (context != null) {
+    showModalBottomSheet<void>(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(16.0),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Event completed',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text('The event was triggered five times '),
+              SizedBox(height: 16.0),
+              Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Close'),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+void showPopupFromAnywhere() {
+  final context = scaffoldKey.currentContext;
+  if (context == null)
+    return;
+  else {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Event Completed'),
+          content: Text('The event was triggered five times'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
 int count = 0;
 void sendTheEvent() {
   // here the event is being sent to the server
   count += 1;
   if (count == 5) {
-    toastMessage("Event triggered, and pop up shown. ");
+    // toastMessage("Event triggered, and pop up shown. ");
+    // showBottomSheetFromAnywhere();
+    showPopupFromAnywhere();
     count = 0;
   }
 }
